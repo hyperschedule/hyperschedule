fc15480e75c41b8afa8d4875c826b0a2426bcd59
Radon Rosborough <radon.neon@gmail.com>
2017-11-12 13:18:27 -0800

    Add course search

--- static/js/hyperschedule.js
+++ static/js/hyperschedule.js
@@ -1,4 +1,76 @@
+const courseSearchInput = document.getElementById('input-search-course-name');
+const courseSearchList = document.getElementById('list-search-courses');
+
 let courseData = null;
+
+function hideEntity(entity)
+{
+  entity.style.display = 'none';
+}
+
+function showEntity(entity)
+{
+  entity.style.display = 'block';
+}
+
+function setEntityVisibility(entity, visible)
+{
+  if (visible)
+  {
+    showEntity(entity);
+  }
+  else
+  {
+    hideEntity(entity);
+  }
+}
+
+function courseToString(course)
+{
+  return course.course_name;
+}
+
+function createCourseEntity(course)
+{
+  const listItem = document.createElement('li');
+  const textNode = document.createTextNode(courseToString(course));
+  listItem.appendChild(textNode);
+  return listItem;
+}
+
+function updateCourseSearchList()
+{
+  while (courseSearchList.hasChildNodes())
+  {
+    courseSearchList.removeChild(courseSearchList.lastChild);
+  }
+  for (let course of courseData.courses)
+  {
+    courseSearchList.appendChild(createCourseEntity(course));
+  }
+  updateCourseSearchResults();
+}
+
+function courseMatchesSearchQuery(course, query)
+{
+  // Case insensitive, with fuzzy whitespace.
+  const regexp = new RegExp(query.replace(/\s+/, '.*'), 'i');
+  return course.course_name.match(regexp);
+}
+
+function updateCourseSearchResults()
+{
+  const query = courseSearchInput.value;
+  const courses = courseData.courses;
+  const entities = courseSearchList.children;
+  for (let idx = 0; idx < courses.length; ++idx)
+  {
+    const course = courses[idx];
+    const entity = entities[idx];
+    const visible = courseMatchesSearchQuery(course, query);
+    setEntityVisibility(entity, visible);
+  }
+}
 
 async function retrieveCourseData()
 {
@@ -9,6 +81,7 @@ async function retrieveCourseData()
                 response.status + ' ' + response.statusText);
   }
   courseData = await response.json();
+  setTimeout(updateCourseSearchList, 0);
 }
 
 async function retrieveCourseDataUntilSuccessful()
@@ -34,4 +107,10 @@ async function retrieveCourseDataUntilSuccessful()
   }
 }
 
+function attachListeners()
+{
+  courseSearchInput.addEventListener('keyup', updateCourseSearchResults);
+}
+
+attachListeners();
 retrieveCourseDataUntilSuccessful();
