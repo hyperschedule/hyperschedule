d12e3212fcb3b190d7bc1773269f200404c92356
Radon Rosborough <radon.neon@gmail.com>
2017-11-18 21:15:37 -0800

    Move scraper and API to separate repo

--- server.js
+++ server.js
@@ -1,129 +1,21 @@
 #!/usr/bin/env node
 
-const child_process = require('mz/child_process');
 const express = require('express');
-const fs = require('mz/fs');
-const moment = require('moment');
 const process = require('process');
 
-let courses = null;
-let production = false;
+const server = express();
 
-async function parseAndSlurpOnce()
-{
-  let result;
-  try
-  {
-    result = await child_process.execFile('./fetch.py');
-  }
-  catch (err)
-  {
-    console.error('Fetch script terminated unexpectedly');
-    console.error(err);
-    throw err;
-  }
-  const jsonString = await fs.readFile('courses.json');
-  courses = JSON.parse(jsonString);
-}
+server.use(express.static('out'));
+server.use((req, res, next) => {
+  console.error('Page not found: ' + req.url);
+  res.status(404).send('Not found');
+});
+server.use((err, req, res, next) => {
+  console.error('Internal server error');
+  console.error(err);
+  res.status(500).send('Internal server error');
+});
 
-async function parseAndSlurpRepeatedly()
-{
-  let originalDelay = 500;
-  let delay = originalDelay;
-  // No exponential backoff during development.
-  const backoffFactor = production ? 1.5 : 1;
-  while (true)
-  {
-    try
-    {
-      await parseAndSlurpOnce();
-      console.log('Fetch script completed successfully');
-      delay = originalDelay;
-    }
-    catch (err)
-    {
-      // network error? try again, with exponential backoff
-      delay *= backoffFactor;
-    }
-    await new Promise(resolve => setTimeout(resolve, delay));
-  }
-}
-
-async function runWebserver()
-{
-  const server = express();
-  server.get('/api/v1/all-courses', (req, res, next) => {
-    if (courses)
-    {
-      const mtime = moment(fs.statSync('courses.json').mtime);
-      const now = moment();
-      const staleness = mtime.from(now);
-      res.json({
-        courses: courses,
-        lastUpdate: staleness,
-      });
-    }
-    else
-    {
-      res.status(500).send('Server could not fetch Portal data');
-    }
-  });
-  const staticDir = production ? 'static/production' : 'static/src';
-  server.use(express.static(staticDir));
-  server.use((req, res, next) => {
-    res.status(404).send('Not found');
-  });
-  server.use((err, req, res, next) => {
-    console.error('Internal server error');
-    console.error(err);
-    res.status(500).send('Internal server error');
-  });
-  const port = process.env.PORT || 5000;
-  await new Promise((resolve, reject) => server.listen(port, err => {
-    if (err)
-    {
-      reject(err);
-    }
-    else
-    {
-      const mode = production ? 'production' : 'dev';
-      console.log(
-        `Hyperschedule webserver (${mode}) listening on port ${port}`);
-      resolve();
-    }
-  }));
-}
-
-async function start()
-{
-  parseAndSlurpRepeatedly();
-  await runWebserver();
-}
-
-function handleCommandLineArguments()
-{
-  // First two arguments are the node binary and the script name.
-  for (let arg of process.argv.slice(2))
-  {
-    if (arg == '--production')
-    {
-      production = true;
-    }
-    else if (arg == '--dev')
-    {
-      production = false;
-    }
-    else
-    {
-      console.error(`Unexpected argument '${arg}', ignoring`);
-    }
-  }
-}
-
-handleCommandLineArguments();
-start()
-  .catch(err => {
-    console.error('Hyperschedule webserver terminated unexpectedly');
-    console.error(err);
-    process.exit(1);
-  });
+const port = process.env.PORT || 5000;
+server.listen(port);
+console.log(`Hyperschedule webapp (dev server) listening on port ${port}`);
