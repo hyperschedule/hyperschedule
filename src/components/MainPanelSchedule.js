dfc4d261df8dc9afda208bbfdc9857b3dfa84999
PythonNut <PythonNut@users.noreply.github.com>
2018-01-11 18:10:35 -0800

    Fix class assignments in MainPanelSchedule

--- src/components/MainPanelSchedule.js
+++ src/components/MainPanelSchedule.js
@@ -22,29 +22,29 @@ const MainPanelSchedule = ({schedule}) => {
     <div>
       <table id="schedule-table">
         <thead>
-          <th class="schedule-hour schedule-hour-0"></th>
-          <th class="schedule-day schedule-day-1">M</th>
-          <th class="schedule-day schedule-day-2">T</th>
-          <th class="schedule-day schedule-day-3">W</th>
-          <th class="schedule-day schedule-day-4">T</th>
-          <th class="schedule-day schedule-day-5">F</th>
+          <th className="schedule-hour schedule-hour-0"></th>
+          <th className="schedule-day schedule-day-1">M</th>
+          <th className="schedule-day schedule-day-2">T</th>
+          <th className="schedule-day schedule-day-3">W</th>
+          <th className="schedule-day schedule-day-4">T</th>
+          <th className="schedule-day schedule-day-5">F</th>
         </thead>
         <tbody id="schedule-table-body">
-          <tr><td class="schedule-hour-1 schedule-hour">8am</td><td></td><td></td><td></td><td></td><td></td></tr>
-          <tr><td class="schedule-hour-2 schedule-hour">9am</td><td></td><td></td><td></td><td></td><td></td></tr>
-          <tr><td class="schedule-hour-3 schedule-hour">10am</td><td></td><td></td><td></td><td></td><td></td></tr>
-          <tr><td class="schedule-hour-4 schedule-hour">11am</td><td></td><td></td><td></td><td></td><td></td></tr>
-          <tr><td class="schedule-hour-5 schedule-hour">12pm</td><td></td><td></td><td></td><td></td><td></td></tr>
-          <tr><td class="schedule-hour-6 schedule-hour">1pm</td><td></td><td></td><td></td><td></td><td></td></tr>
-          <tr><td class="schedule-hour-7 schedule-hour">2pm</td><td></td><td></td><td></td><td></td><td></td></tr>
-          <tr><td class="schedule-hour-8 schedule-hour">3pm</td><td></td><td></td><td></td><td></td><td></td></tr>
-          <tr><td class="schedule-hour-9 schedule-hour">4pm</td><td></td><td></td><td></td><td></td><td></td></tr>
-          <tr><td class="schedule-hour-10 schedule-hour">5pm</td><td></td><td></td><td></td><td></td><td></td></tr>
-          <tr><td class="schedule-hour-11 schedule-hour">6pm</td><td></td><td></td><td></td><td></td><td></td></tr>
-          <tr><td class="schedule-hour-12 schedule-hour">7pm</td><td></td><td></td><td></td><td></td><td></td></tr>
-          <tr><td class="schedule-hour-13 schedule-hour">8pm</td><td></td><td></td><td></td><td></td><td></td></tr>
-          <tr><td class="schedule-hour-14 schedule-hour">9pm</td><td></td><td></td><td></td><td></td><td></td></tr>
-          <tr><td class="schedule-hour-15 schedule-hour">10pm</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-1 schedule-hour">8am</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-2 schedule-hour">9am</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-3 schedule-hour">10am</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-4 schedule-hour">11am</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-5 schedule-hour">12pm</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-6 schedule-hour">1pm</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-7 schedule-hour">2pm</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-8 schedule-hour">3pm</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-9 schedule-hour">4pm</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-10 schedule-hour">5pm</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-11 schedule-hour">6pm</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-12 schedule-hour">7pm</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-13 schedule-hour">8pm</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-14 schedule-hour">9pm</td><td></td><td></td><td></td><td></td><td></td></tr>
+          <tr><td className="schedule-hour-15 schedule-hour">10pm</td><td></td><td></td><td></td><td></td><td></td></tr>
         </tbody>
         {entities}
       </table>
