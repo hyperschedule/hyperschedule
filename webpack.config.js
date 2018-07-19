3d3965ba53bde004ec97615b7d012ea29a367b24
Kye W. Shi <shi.kye@gmail.com>
2018-07-18 18:54:35 -0700

    Update some package configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,50 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: ['babel-polyfill', './src'],
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'public', 'static'),
-    publicPath: 'static',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            'env',
-            'react',
-          ],
-          plugins: [
-            'babel-plugin-transform-class-properties',
-          ],
-        },
-      },
-      {
-        test: /\.css?$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ]
-      },
-      {
-        test: /\.(svg|woff2?|png|eot|ttf)$/,
-        loader: 'file-loader',
-      },
-    ],
-  },
-  resolve: {
-    modules: [
-      'node_modules',
-      path.join(__dirname, 'src'),
-    ],
-    extensions: ['.js', '.jsx'],
-  },
+    mode: 'development',
+    entry: ['babel-polyfill', './src'],
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public', 'static'),
+        publicPath: 'static',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                include: [
+                    path.join(__dirname, 'src'),
+                ],
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        'env',
+                        'react',
+                    ],
+                    plugins: [
+                        'transform-class-properties',
+                        'transform-object-rest-spread',
+                    ],
+                },
+            },
+            {
+                test: /\.css?$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            },
+            {
+                test: /\.(svg|woff2?|png|eot|ttf)$/,
+                loader: 'file-loader',
+            },
+        ],
+    },
+    resolve: {
+        modules: [
+            'node_modules',
+            path.join(__dirname, 'src'),
+        ],
+        extensions: ['.js', '.jsx'],
+    },
 };
