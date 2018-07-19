9217ab2c84ea053349dc562b0210ff74627e4418
Kye W. Shi <shi.kye@gmail.com>
2018-07-19 11:12:42 -0700

    Implement animated resizing on focused course summary

--- src/App/CoursePanel/CoursePanel.jsx
+++ src/App/CoursePanel/CoursePanel.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
+import Measure from 'react-measure';
+
 import './CoursePanel.css';
 
 //import {SortableContainer, SortableElement} from 'react-sortable-hoc';
@@ -23,78 +25,104 @@ import './CoursePanel.css';
 //  }
 //);
 
-const CoursePanel = ({focusCourse}) => {
-    let focusSummary = null;
-    if (focusCourse !== null) {
-        const schedule = focusCourse.get('schedule');
-        const faculty = focusCourse.get('faculty');
+class CoursePanel extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            focusSummaryHeight: 0
+        };
+    }
+
+    render() {
+        const {focusCourse} = this.props;
         
-        const scheduleBlocks = schedule.map((block, index) => (
-            <div key={index} className="block">
-              <span className="days">
-                {block.get('days')}
-              </span>
-              <span className="start-time">
-                {block.get('startTime')}
-              </span>
-              <span className="end-time">
-                {block.get('endTime')}
-              </span>
-              <span className="location">
-                {block.get('location')}
-              </span>
-            </div>
-        ));
-        
-        focusSummary = (
-            <div className="focus-summary">
-              <div className="row description">
-                <span className="department">
-                  {focusCourse.get('department')}
-                </span>
-                <span className="course-number">
-                  {focusCourse.get('courseNumber')}
-                </span>
-                <span className="course-code-suffix">
-                  {focusCourse.get('courseCodeSuffix')}
-                </span>
-                <span className="school">
-                  {focusCourse.get('school')}
-                </span>
-                <span className="section">
-                  {focusCourse.get('section').toString().padStart(2, '0')}
-                </span>
-                <span className="course-name">
-                  {focusCourse.get('courseName')}
-                </span>
-              </div>
-              {schedule.size === 0 ? null : (
-                  <div className="row schedule">
-                    {scheduleBlocks}
+        let focusSummary = null;
+        if (focusCourse !== null) {
+            const schedule = focusCourse.get('schedule');
+            const faculty = focusCourse.get('faculty');
+            
+            const scheduleBlocks = schedule.map((block, index) => (
+                <div key={index} className="block">
+                  <span className="days">
+                    {block.get('days')}
+                  </span>
+                  <span className="start-time">
+                    {block.get('startTime')}
+                  </span>
+                  <span className="end-time">
+                    {block.get('endTime')}
+                  </span>
+                  <span className="location">
+                    {block.get('location')}
+                  </span>
+                </div>
+            ));
+            
+            focusSummary = (
+                <div className="focus-summary">
+                  <div className="row description">
+                    <span className="department">
+                      {focusCourse.get('department')}
+                    </span>
+                    <span className="course-number">
+                      {focusCourse.get('courseNumber')}
+                    </span>
+                    <span className="course-code-suffix">
+                      {focusCourse.get('courseCodeSuffix')}
+                    </span>
+                    <span className="school">
+                      {focusCourse.get('school')}
+                    </span>
+                    <span className="section">
+                      {focusCourse.get('section').toString().padStart(2, '0')}
+                    </span>
+                    <span className="course-name">
+                      {focusCourse.get('courseName')}
+                    </span>
                   </div>
-              )}
-              <div className="row faculty">
-                {
-                    faculty.size === 1 ? (
-                        faculty.get(0)
-                    ) : faculty.size === 2 ? (
-                        faculty.join(' and ')
-                    ) : faculty.size === 3 ? (
-                        faculty.slice(0, -1).join(', ') + ', and ' + faculty.get(-1)
-                    ) : null
-                }
+                  {schedule.size === 0 ? null : (
+                      <div className="row schedule">
+                        {scheduleBlocks}
                       </div>
+                  )}
+                    <div className="row faculty">
+                    {
+                        faculty.size === 1 ? (
+                            faculty.get(0)
+                        ) : faculty.size === 2 ? (
+                            faculty.join(' and ')
+                        ) : faculty.size === 3 ? (
+                            faculty.slice(0, -1).join(', ') + ', and ' + faculty.get(-1)
+                        ) : null
+                    }
+                </div>
+                    </div>
+            );
+        }
+
+        const updateHeight = ({offset}) => this.setState({
+            focusSummaryHeight: offset.height,
+        });
+        
+        const focusSummaryMeasure = ({measureRef}) => (
+            <div ref={measureRef} className="focus-summary-measure">
+              {focusSummary}
+            </div>
+        );
+        
+        return (
+            <div id="course-panel">
+              <div className="focus-summary-container" style={{
+                       height: this.state.focusSummaryHeight + 'px',
+                   }}>
+                <Measure offset onResize={updateHeight}>
+                  {focusSummaryMeasure}
+                </Measure>
+              </div>
             </div>
         );
     }
-
-    return (
-        <div id="course-panel">
-          <div className="focus-summary-container">
-            {focusSummary}
-          </div>
-        </div>
-    );
 };
 
 const CoursePanelWrapper = connect(
