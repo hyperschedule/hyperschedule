9b22dafa115ab41e537e24f87e5572790473d2cb
Kye W. Shi <shi.kye@gmail.com>
2018-08-04 17:51:43 -0700

    Install some packages for ical

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,56 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: ['babel-polyfill', './src'],
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'public', 'static'),
-        publicPath: 'static',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                include: [
-                    path.join(__dirname, 'src'),
-                ],
-                loader: 'babel-loader',
-                options: {
-                    retainLines: true,
-                    presets: [
-                        'env',
-                        'react',
-                    ],
-                    plugins: [
-                        'transform-class-properties',
-                        'transform-object-rest-spread',
-                    ],
-                },
-            },
-            {
-                test: /\.css?$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ]
-            },
-            {
-                test: /\.(svg|woff2?|png|eot|ttf)$/,
-                loader: 'file-loader',
-            },
+  mode: 'development',
+  entry: ['babel-polyfill', './src'],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public', 'static'),
+    publicPath: 'static',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.join(__dirname, 'src'),
         ],
-    },
+        loader: 'babel-loader',
+        options: {
+          retainLines: true,
+          presets: [
+            'env',
+            'react',
+          ],
+          plugins: [
+            'transform-class-properties',
+            'transform-object-rest-spread',
+          ],
+        },
+      },
+      {
+        test: /\.css?$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+      {
+        test: /\.(svg|woff2?|png|eot|ttf)$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
 	node: {
 		fs: 'empty',
+    dns: 'mock',
+    net: 'mock',
 	},
-    resolve: {
-        modules: [
-            path.join(__dirname, 'src'),
-            path.join(__dirname, 'node_modules'),
-        ],
-        extensions: ['.js', '.jsx'],
-    },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'src'),
+      path.join(__dirname, 'node_modules'),
+    ],
+    extensions: ['.js', '.jsx'],
+  },
 };
