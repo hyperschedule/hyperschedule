3f0b9682c1b308c37831be75668dc0e51ab6fb3e
Kye W. Shi <shi.kye@gmail.com>
2018-08-11 01:55:04 -0700

    Skip initial ALL_COURSES ping

--- src/sagas.jsx
+++ src/sagas.jsx
@@ -11,8 +11,8 @@ import {Map, OrderedMap, fromJS} from 'immutable';
 const API_UPDATE_PERIOD_MS = 1000 * 30;
 
 function* periodicApiUpdate() {
-  const {courses, timestamp} = yield call(api.allCourses);
-  yield put(actions.allCourses(courses, timestamp));
+  //const {courses, timestamp} = yield call(api.allCourses);
+  //yield put(actions.allCourses(courses, timestamp));
 
   for (;;) {
     const prevTimestamp = yield select(state => state.getIn(['api', 'timestamp']));
