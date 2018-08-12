0296c2f665bd3f293d243fd147da6fdcc7fab037
Kye W. Shi <shi.kye@gmail.com>
2018-08-12 00:16:55 -0700

    Implement simple saga-based localStorage write

--- src/sagas.jsx
+++ src/sagas.jsx
@@ -1,16 +1,18 @@
-import {call, put, select} from 'redux-saga/effects';
+import {fork, call, put, select, takeEvery} from 'redux-saga/effects';
 import {delay} from 'redux-saga';
+
+import Mode from '@/App/mode';
 
 import * as api from './api';
 
-import * as util from '@/util/hyperschedule-util';
+import * as serializeUtil from '@/util/serialize';
 
 import * as actions from './actions';
 import {Map, OrderedMap, fromJS} from 'immutable';
 
 const API_UPDATE_PERIOD_MS = 1000 * 30;
 
-function* periodicApiUpdate() {
+function* periodicAPI() {
   //const {courses, timestamp} = yield call(api.allCourses);
   //yield put(actions.allCourses(courses, timestamp));
 
@@ -30,4 +32,50 @@ function* periodicApiUpdate() {
   }
 }
 
-export default periodicApiUpdate;
+function* persistAPI() {
+  const api = yield select(state => state.get('api'));
+
+  const courses = api.get('courses');
+  const data = api.get('order').map(key => courses.get(key)).toJS();
+  
+  localStorage.setItem(
+    'courseList',
+    JSON.stringify(data),
+  );
+  
+  localStorage.setItem(
+    'courseDataTimestamp',
+    JSON.stringify(api.get('timestamp')),
+  );
+}
+
+function* persistMode() {
+  const mode = yield select(state => state.get('mode'));
+  localStorage.setItem(
+    'scheduleTabSelected',
+    JSON.stringify(mode === Mode.SCHEDULE),
+  );
+}
+
+function* persistSelection() {
+  const selection = yield select(state => state.get('selection'));
+  localStorage.setItem(
+    'selectedCourses',
+    JSON.stringify(serializeUtil.serializeSelection(selection)),
+  );
+}
+
+export default function* () {
+  yield fork(periodicAPI);
+
+  yield takeEvery([actions.ALL_COURSES, actions.COURSES_SINCE], persistAPI);
+  yield takeEvery([actions.modeSelector.SET_MODE], persistMode);
+  yield takeEvery([
+    actions.courseSearch.ADD_COURSE,
+    actions.selectedCourses.REMOVE_COURSE,
+    actions.selectedCourses.TOGGLE_COURSE_CHECKED,
+    actions.selectedCourses.TOGGLE_COURSE_STARRED,
+  ], persistSelection);
+  
+}
+
