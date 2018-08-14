06594897582ec8bf7ac3465f26c21bfcd355eaae
Radon Rosborough <radon.neon@gmail.com>
2018-08-13 20:06:46 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+#!/usr/bin/env node
+
 const express = require('express');
+const process = require('process');
 
-const app = express();
+const server = express();
 
-app.use('/', express.static('public'));
+server.use(express.static('public'));
+server.use((req, res, next) => {
+  console.error('Page not found: ' + req.url);
+  res.status(404).send('Not found');
+});
+server.use((err, req, res, next) => {
+  console.error('Internal server error');
+  console.error(err);
+  res.status(500).send('Internal server error');
+});
 
-console.log('Starting');
-app.listen(3000);
+const port = process.env.PORT || 5000;
+server.listen(port);
+console.log(`Hyperschedule webapp (dev server) listening on port ${port}`);
