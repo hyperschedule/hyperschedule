f8403d21003c6b9064f0684a2ba5db5d9661fac7
Kye W. Shi <shi.kye@gmail.com>
2018-08-24 16:23:54 -0700

    Add sort key comparator higher-order function for course sorting

--- src/util/misc.jsx
+++ src/util/misc.jsx
@@ -41,14 +41,24 @@ export function commaJoin(items, comma = ',') {
   }
 };
 
-function compareDefault(a, b) {
-  return a < b ? -1 : a > b ? 1 : 0;
+// Produces a comparator function from a sort-key function.
+export function sortKeyComparator(key = item => item) {
+  return (a, b) => {
+    const keyA = key(a),
+          keyB = key(b);
+
+    switch (true) {
+    case keyA < keyB: return -1;
+    case keyA > keyB: return 1;
+    default:          return 0;
+    }
+  };
 }
 
 // Searches a sorted Immutable.List to find the index for inserting an item.
 // This is used to incrementally insert or remove items from the
 // (sorted) course list.
-export function binarySearch(list, item, compare = compareDefault) {
+export function binarySearch(list, item, compare = sortKeyComparator()) {
   let left = 0,
       right = list.size;
 
