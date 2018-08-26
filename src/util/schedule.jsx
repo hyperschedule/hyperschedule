43bba2668b99133fcdfef9951050f724a73cf8d4
Kye W. Shi <shi.kye@gmail.com>
2018-08-26 01:52:28 -0700

    Rewrite computeSchedule to avoid let declarations

--- src/util/schedule.jsx
+++ src/util/schedule.jsx
@@ -7,40 +7,36 @@ export function computeSchedule(selection) {
     checked = selection.get("checked"),
     order = selection.get("order");
 
-  let schedule = Set();
-
-  for (const key of order) {
-    if (checked.has(key) && starred.has(key)) {
-      schedule = schedule.add(key);
-    }
-  }
-
-  for (const key of order) {
-    if (!checked.has(key)) {
-      continue;
-    }
-
+  function scheduleCompatible(schedule, key) {
     const course = courses.get(key);
 
-    let conflictFound = false;
-    for (const otherKey of schedule) {
-      const other = courses.get(otherKey);
+    for (const existingKey of schedule) {
+      const other = courses.get(existingKey);
 
       if (
         courseUtil.coursesConflict(course, other) ||
         courseUtil.coursesEquivalent(course, other)
       ) {
-        conflictFound = true;
-        break;
+        return false;
       }
     }
 
-    if (conflictFound) {
-      continue;
-    }
-
-    schedule = schedule.add(key);
+    return true;
   }
 
-  return schedule;
+  const scheduleStarred = order.reduce(
+    (schedule, key) =>
+      checked.has(key) && starred.has(key)
+        ? schedule.add(key)
+        : schedule,
+    Set(),
+  );
+
+  return order.reduce((schedule, key) => {
+    if (!checked.has(key) || !scheduleCompatible(schedule, key)) {
+      return schedule;
+    }
+
+    return schedule.add(key);
+  }, scheduleStarred);
 }
