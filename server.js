f4f1cd01e9651c71721421e785b5a68fa31cc40e
Kye W. Shi <shi.kye@gmail.com>
2018-08-27 02:22:39 -0700

    Modify server.js to comply to eslint

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 #!/usr/bin/env node
 
-const express = require('express');
-const process = require('process');
+/* eslint-disable no-console */
+
+const express = require("express");
+const process = require("process");
 
 const server = express();
 
-server.use(express.static('public'));
-server.use((req, res, next) => {
-  console.error('Page not found: ' + req.url);
-  res.status(404).send('Not found');
+server.use(express.static("public"));
+server.use((req, res) => {
+  console.error("Page not found: " + req.url);
+  res.status(404).send("Not found");
 });
-server.use((err, req, res, next) => {
-  console.error('Internal server error');
+server.use((err, req, res) => {
+  console.error("Internal server error");
   console.error(err);
-  res.status(500).send('Internal server error');
+  res.status(500).send("Internal server error");
 });
 
 const port = process.env.PORT || 5000;
 server.listen(port);
-console.log(`Hyperschedule webapp (dev server) listening on port ${port}`);
+console.log(
+  `Hyperschedule webapp (dev server) listening on port ${port}`,
+);
