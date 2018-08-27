e18ce0ba2f28d9b44f74b1319e0c2728d28e88a7
Kye W. Shi <shi.kye@gmail.com>
2018-08-27 01:48:06 -0700

    pre-commit hook: Add support for JSON auto-linting based on extension

--- pre-commit-lint.bash
+++ pre-commit-lint.bash
@@ -10,16 +10,32 @@ fail=0
 for file in "$@"; do
     temp_record=$(git checkout-index --temp "$file")
     temp_file=$(echo "$temp_record" | cut -f 1)
-    echo "linting '$file'..."
-    yarn eslint --quiet --no-ignore "$temp_file"
-    if ! [ $? -eq 0 ]; then
-        echo "lint errors found!"
+
+    ext=${file##*.}
+
+    code=0
+    case "$ext" in
+        json)
+            yarn prettier --parser json --list-different "$temp_file"
+            code=$?
+            ;;
+        js|jsx)
+            yarn eslint --quiet --no-ignore "$temp_file"
+            code=$?
+            ;;
+        *)
+    esac
+
+    if ! [ $code -eq 0 ]; then
+        echo "Lint errors found for '$file'!"
         fail=$((fail + 1))
     fi
+
     rm "$temp_file"
+
 done
 
 if [ $fail -gt 0 ]; then
-    echo "lint errors found for $fail files!"
+    echo "Lint errors found for $fail file(s)!"
     exit 2
 fi
