3ccd90e970e653b39630555fc64466ce3f6ec390
Kye W. Shi <shi.kye@gmail.com>
2020-09-29 19:54:26 -0700

    use single config; enable hot-reloading

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env, argv) => {
+  const prod = argv.mode === "production";
+
+  return {
+    mode: argv.mode,
+    entry: {
+      app: path.join(__dirname, "src/js/hyperschedule.ts")
+    },
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "hyperschedule.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/i,
+          use: "ts-loader"
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: !prod
+              }
+            },
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    "postcss-nested",
+                    "autoprefixer",
+                    "postcss-validator"
+                  ]
+                }
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "hyperschedule",
+        template: "src/index.html"
+      }),
+      new MiniCssExtractPlugin()
+    ],
+    devtool: prod ? "hidden-source-map" : "eval-source-map",
+    devServer: {
+      host: "0.0.0.0",
+      contentBase: path.join(__dirname, "dist"),
+      port: 5000,
+      hot: true
+    }
+  };
+};
--- webpack.prod.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-const { merge } = require("webpack-merge");
-
-module.exports = merge(require("./webpack.common.js"), {
-  mode: "production",
-  devtool: "hidden-source-map"
-});
