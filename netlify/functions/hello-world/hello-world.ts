158dbf97c915710ada38037df3d3acb74264fe0a
Kye Shi <shi.kye@gmail.com>
2021-08-23 12:56:58 -0700

    some fixes

--- netlify/functions/hello-world/hello-world.ts
+++ netlify/functions/hello-world/hello-world.ts
@@ -1,12 +1,13 @@
-import { Handler } from '@netlify/functions'
+import { Handler } from "@netlify/functions";
 
 export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters
+  const { name = "stranger" }: { name?: string } =
+    event.queryStringParameters || {};
 
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: `Hello, ${name}!`,
     }),
-  }
-}
+  };
+};
