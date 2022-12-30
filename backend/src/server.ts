cb7406fab3cd054297662db88ae337b4d1d60477
Mia Celeste <mia@miaceleste.dev>
2022-12-30 13:26:03 -0800

    Added dummy server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,22 @@
 import fastify from "fastify";
 
 const server = fastify();
+import fs from "fs";
 
-server.get("courses", async (request, response) => {});
+const courses = JSON.parse(
+    fs.readFileSync("./src/hmc-api/sample/parsed-sample-v4.json", {
+        encoding: "utf-8",
+    }),
+);
+
+server.get("/v4/courses", async (request, response) => {
+    return courses;
+});
+
+server.listen({ port: 8080 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(0);
+    }
+    console.log(`Server listening at ${address}`);
+});
