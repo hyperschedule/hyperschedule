fdec2bc210195b166d25a57e794d58921f608f43
Kye W. Shi <shi.kye@gmail.com>
2023-01-07 10:01:45 -0800

    frontend: add useStore hook via zustand

---
+++ frontend/src/hooks/store.ts
@@ -0,0 +1,29 @@
+import createStore from "zustand";
+import type * as Search from "@lib/search";
+
+export interface Store {
+    mainTab: MainTab;
+    setMainTab: (tab: MainTab) => void;
+
+    search: {
+        text: string;
+        setText: (text: string) => void;
+        filters: Search.Filter[];
+    };
+}
+
+export const enum MainTab {
+    CourseSearch,
+    Schedule,
+}
+
+export default createStore<Store>((set, get) => ({
+    // TODO: are nested stores a thing? check zustand docs
+    mainTab: MainTab.CourseSearch,
+    setMainTab: (mainTab) => set({ mainTab }),
+    search: {
+        text: "",
+        setText: (text) => set({ search: { ...get().search, text } }),
+        filters: [],
+    },
+}));
