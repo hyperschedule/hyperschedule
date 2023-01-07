b1acfb3e7a8d318383e8842befb9abce95310f2c
Kye W. Shi <shi.kye@gmail.com>
2023-01-07 10:01:33 -0800

    frontend: add basic (text-only) searching logic

---
+++ frontend/src/lib/search.ts
@@ -0,0 +1,67 @@
+import type { Section } from "hyperschedule-shared/api/v4";
+
+export const enum FilterKey {
+    Department,
+    Title,
+    Description,
+    CourseCode,
+    Instructor,
+    ScheduleDays,
+}
+
+export type Filter =
+    | {
+          key:
+              | FilterKey.Department
+              | FilterKey.Instructor
+              | FilterKey.Description
+              | FilterKey.CourseCode
+              | FilterKey.Title;
+          contains: string;
+      }
+    | {
+          key: FilterKey.ScheduleDays;
+          days: Set<never>; // TODO
+      };
+
+export interface Search {
+    text: string;
+    filters: Filter[];
+}
+
+export function matchesFilter(section: Section, filter: Filter): boolean {
+    switch (filter.key) {
+        case FilterKey.Department:
+            return false;
+
+        default:
+            return false;
+    }
+}
+
+export function matchesText(section: Section, text: string): boolean {
+    const terms: string[] = [];
+    for (const match of text.matchAll(/[A-Za-z]+|\d+/g))
+        terms.push(match[0].toLocaleLowerCase());
+
+    const items = [
+        section.course.code.department,
+        section.course.code.courseNumber.toString().padStart(3, "0"),
+        section.course.code.affiliation,
+        section.identifier.sectionNumber.toString().padStart(2, "0"),
+        section.course.title,
+    ];
+
+    return terms.every((term) =>
+        items.some((item) =>
+            item.toLocaleLowerCase().includes(term.toLocaleLowerCase()),
+        ),
+    );
+}
+
+export function matches(section: Section, search: Search): boolean {
+    return (
+        matchesText(section, search.text) &&
+        search.filters.every((filter) => matchesFilter(section, filter))
+    );
+}
