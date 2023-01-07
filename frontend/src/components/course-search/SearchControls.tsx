7d5986c88a33857ad4882f1495ac875fc8686df1
Kye W. Shi <shi.kye@gmail.com>
2023-01-07 10:07:50 -0800

    frontend: attach SearchControls text input to global store

--- frontend/src/components/course-search/SearchControls.tsx
+++ frontend/src/components/course-search/SearchControls.tsx
@@ -1,10 +1,17 @@
 import * as Css from "./SearchControls.module.css";
 import * as Feather from "react-feather";
 
+import useStore from "@hooks/store";
+
 export default function SearchControls() {
+    const search = useStore((store) => store.search);
     return (
         <div className={Css.searchControls}>
-            <input placeholder="Search for courses..." />
+            <input
+                value={search.text}
+                onChange={(ev) => search.setText(ev.currentTarget.value)}
+                placeholder="Search for courses..."
+            />
             <button className={Css.filterButton}>
                 <Feather.Sliders size={16} />
                 Filters
