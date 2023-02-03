e954de050226ad47953581e4227dfbe959b02f84
Kye Shi <shi.kye@gmail.com>
2023-02-02 22:18:31 -0800

    add test plus one route for typed api routes

--- shared/api/routes.ts
+++ shared/api/routes.ts
@@ -1,15 +1,24 @@
 import * as Routing from "hyperschedule-shared/lib/routing";
 import * as Zod from "zod";
 
-export default {
-    v3: {},
-    v4: {
-        sections: Routing.get("v4/sections", {}),
-        sectionsBySemester: Routing.get("v4/sections/:term/:year", {
-            params: {
-                term: Zod.number(),
-                year: Zod.number(),
-            },
-        }),
-    },
-};
+export default Routing.modules({
+    v3: Routing.endpoints({}),
+    v4: Routing.endpoints({
+        testPlusOne: Routing.get(
+            "test/plus-one/:num",
+            { param: { num: Zod.number() } },
+            { 200: Zod.number() },
+        ),
+        //sections: Routing.get("v4/sections", {}, { 200: Zod.void() }),
+        //sectionsBySemester: Routing.get(
+        //    "v4/sections/:term/:year",
+        //    {
+        //        param: {
+        //            term: Zod.number(),
+        //            year: Zod.number(),
+        //        },
+        //    },
+        //    { 200: Zod.void() },
+        //),
+    }),
+});
