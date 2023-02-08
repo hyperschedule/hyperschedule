f42c1850d39971e9e4f5b5b4514a8947dc56df70
Mia Celeste <mia@miaceleste.dev>
2023-02-07 16:40:22 -0800

    backend: added logging for response headers and removed x-powered-by

--- backend/src/routes/v4/courses.ts
+++ backend/src/routes/v4/courses.ts
@@ -3,7 +3,7 @@ import { Term } from "hyperschedule-shared/api/v4";
 import { getAllSections } from "../../db/models/course";
 import { App } from "@tinyhttp/app";
 
-const courseApp = new App();
+const courseApp = new App({ settings: { xPoweredBy: false } });
 
 const validTerms: string[] = Object.values(Term);
 
