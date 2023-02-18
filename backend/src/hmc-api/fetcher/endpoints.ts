ca5d696b4e1b3bacef695312a856141a53a99163
Mia Celeste <mia@miaceleste.dev>
2023-02-18 02:22:02 -0800

    backend: added more details for endpoints.ts

--- backend/src/hmc-api/fetcher/endpoints.ts
+++ backend/src/hmc-api/fetcher/endpoints.ts
@@ -8,6 +8,10 @@
  *
  * Future maintainers of hyperschedule: if you have root access to our server, you may
  * find an actual copy of this file on the server.
+ *
+ * If you have modified this file, you may exclude it by running
+ * `git update-index --skip-worktree backend/src/hmc-api/fetcher/endpoints.ts`
+ * on your local repository to exclude it from being modified
  */
 
 export { endpoints } from "./endpoints-dev";
