9e7c90c78dbe14b1e0a9818b37c0bb8f59cabb10
Mia Celeste <mia@miaceleste.dev>
2023-02-18 20:43:21 -0800

    backend: added working fetcher

---
+++ backend/tests/hmc-fetcher.test.ts
@@ -0,0 +1,40 @@
+import { describe, test, expect } from "@jest/globals";
+import * as APIv4 from "hyperschedule-shared/api/v4";
+import { computeParams } from "../src/hmc-api/fetcher/utils";
+
+describe("src/hmc-api/fetcher/utils.ts", () => {
+    test("computeParams", () => {
+        expect(
+            computeParams({
+                year: 2023,
+                term: APIv4.Term.spring,
+            }),
+        ).toEqual({
+            year: "2023",
+            catalog: "UG22",
+            session: "SP2023",
+        });
+
+        expect(
+            computeParams({
+                year: 2023,
+                term: APIv4.Term.summer,
+            }),
+        ).toEqual({
+            year: "2023",
+            catalog: "UG22",
+            session: "SU2023",
+        });
+
+        expect(
+            computeParams({
+                year: 2023,
+                term: APIv4.Term.fall,
+            }),
+        ).toEqual({
+            year: "2023",
+            catalog: "UG23",
+            session: "FA2023",
+        });
+    });
+});
