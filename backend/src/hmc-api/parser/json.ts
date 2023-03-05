b023ca185905ee4b66589af759e0ece43d4cc9b0
Mia Celeste <mia@miaceleste.dev>
2023-03-04 18:41:59 -0800

    backend: json parser skeleton complete

---
+++ backend/src/hmc-api/parser/json.ts
@@ -0,0 +1,88 @@
+/**
+ * This module is the parser for json object we receive. The simple way to use it is to specify input, a zod validator
+ * for output, and a transform object with functions for each key. parseJSON is the array equivalent of parseJSONItem.
+ * For example:
+ *
+ *
+ */
+
+import { createLogger } from "../../logger";
+import type { z } from "zod";
+
+const logger = createLogger("hmc.parser.json");
+
+export interface TransformResult<Output, Key extends keyof Output> {
+    name: Key;
+    value: Output[Key];
+}
+
+export type TransformFunction<
+    Input,
+    InKey extends keyof Input,
+    Output,
+    OutKey extends keyof Output,
+> = (v: Input[InKey]) => TransformResult<Output, OutKey>;
+
+export const NoTransform = Symbol();
+export type NoTransform = typeof NoTransform;
+
+export const Remove = Symbol();
+export type Remove = typeof Remove;
+
+export type JSONTransform<Input, Output> = Record<
+    keyof Input,
+    | TransformFunction<Input, keyof Input, Output, keyof Output>
+    | NoTransform
+    | Remove
+>;
+type Data = Record<string, unknown>;
+
+/**
+ * Parses a single json item result with transform.
+ * @param data the input data. this data should be valid and already sanitized with zod
+ * @param outValidator the zod object for the output
+ * @param transform an mapping of key to transform functions to transform and rename the data
+ */
+export function parseJSONItem<
+    Input extends Data,
+    Output extends Data,
+    ZodShape extends z.ZodRawShape,
+>(
+    data: Input,
+    outValidator: z.ZodType<Output, z.ZodObjectDef<ZodShape, "strict">>,
+    transform: JSONTransform<Input, Output>,
+): z.SafeParseReturnType<Output, Output> {
+    const result: Partial<Output> = {};
+    for (const inKey of Object.keys(transform) as (keyof Input)[]) {
+        const transformFunc = transform[inKey];
+        if (transformFunc === NoTransform) {
+            // this is fine because we also sanitize the data with zod before returning it
+            (result as any)[inKey] = data[inKey];
+        } else if (transformFunc === Remove) {
+            // do nothing
+        } else {
+            const { name, value } = transformFunc(data[inKey]);
+            result[name] = value;
+        }
+    }
+
+    return outValidator.safeParse(result);
+}
+
+export function parseJSON<
+    Input extends Data,
+    Output extends Data,
+    ZodShape extends z.ZodRawShape,
+>(
+    data: Input[],
+    outValidator: z.ZodType<Output, z.ZodObjectDef<ZodShape, "strict">>,
+    transform: JSONTransform<Input, Output>,
+): Output[] {
+    const result: Output[] = [];
+    for (const obj of data) {
+        const parsed = parseJSONItem(obj, outValidator, transform);
+        if (parsed.success) result.push(parsed.data);
+        else logger.warn("Cannot parse object %O", obj);
+    }
+    return result;
+}
---
+++ backend/tests/hmc-api/json-parser.test.ts
@@ -0,0 +1,162 @@
+import { describe, expect, test } from "@jest/globals";
+import {
+    NoTransform,
+    parseJSONItem,
+    Remove,
+} from "../../src/hmc-api/parser/json";
+import type { SafeParseSuccess } from "zod";
+import { z } from "zod";
+
+describe("hmc-api/parser/json.ts", () => {
+    test("Single json item parsing without transform", () => {
+        const out = parseJSONItem(
+            { a: 1, b: 2 },
+            z.object({ a: z.number(), b: z.string() }).strict(),
+            {
+                a: NoTransform,
+                b: NoTransform,
+            },
+        );
+        expect(out.success).toBeFalsy();
+
+        const out2 = parseJSONItem(
+            { a: 1, b: 2 },
+            z.object({ a: z.number(), b: z.number() }).strict(),
+            {
+                a: NoTransform,
+                b: NoTransform,
+            },
+        );
+        expect(out2.success).toBeTruthy();
+        expect((out2 as SafeParseSuccess<unknown>).data).toStrictEqual({
+            a: 1,
+            b: 2,
+        });
+    });
+
+    test("Single json item parsing with extra field", () => {
+        const out = parseJSONItem(
+            { a: 1, b: 2 },
+            z.object({ a: z.number() }).strict(),
+            {
+                a: NoTransform,
+                b: Remove,
+            },
+        );
+        expect(out.success).toBeTruthy();
+    });
+
+    test("Single json item parsing with value transform", () => {
+        const out = parseJSONItem(
+            { a: 1, b: 2 },
+            z.object({ a: z.number(), b: z.string() }).strict(),
+            {
+                a: NoTransform,
+                b(v: number) {
+                    return {
+                        name: "b",
+                        value: v.toString(),
+                    };
+                },
+            },
+        );
+        expect(out.success).toBeTruthy();
+        expect((out as SafeParseSuccess<unknown>).data).toStrictEqual({
+            a: 1,
+            b: "2",
+        });
+    });
+
+    test("Single json item parsing with incorrect transform", () => {
+        const out = parseJSONItem(
+            { a: 1, b: 2 },
+            z.object({ a: z.number(), b: z.number() }).strict(),
+            {
+                a: NoTransform,
+                b(v: number) {
+                    return {
+                        name: "a",
+                        value: v,
+                    };
+                },
+            },
+        );
+        expect(out.success).toBeFalsy();
+
+        const out2 = parseJSONItem(
+            { a: 1, b: 2 },
+            z.object({ a: z.number(), b: z.number() }).strict(),
+            {
+                a: NoTransform,
+                // @ts-expect-error non-existent key c
+                b(v: number) {
+                    return {
+                        name: "c",
+                        value: v,
+                    };
+                },
+            },
+        );
+        expect(out2.success).toBeFalsy();
+
+        const out3 = parseJSONItem(
+            { a: 1, b: 2 },
+            z.object({ a: z.number(), b: z.number() }).strict(),
+            {
+                a: NoTransform,
+                // @ts-expect-error wrong return type for v
+                b(v: number) {
+                    return {
+                        name: "b",
+                        value: v.toString(),
+                    };
+                },
+            },
+        );
+        expect(out3.success).toBeFalsy();
+
+        const out4 = parseJSONItem(
+            { a: 1, b: 2 },
+            z.object({ a: z.string(), b: z.boolean() }).strict(),
+            {
+                a: NoTransform,
+                b(v: number) {
+                    return {
+                        name: "b",
+                        value: v.toString(),
+                    };
+                },
+            },
+        );
+        expect(out4.success).toBeFalsy();
+
+        const out5 = parseJSONItem(
+            { a: 1, b: 2, c: 5 },
+            z.object({ a: z.string(), b: z.boolean() }).strict(),
+            {
+                a: NoTransform,
+                b(v: number) {
+                    return {
+                        name: "b",
+                        value: v.toString(),
+                    };
+                },
+                c: NoTransform,
+            },
+        );
+        expect(out5.success).toBeFalsy();
+    });
+
+    test("zod validator has to be strict", () => {
+        const result = parseJSONItem(
+            { a: 1, b: 2 },
+            // @ts-expect-error z.object is not in strict mode
+            z.object({ a: z.number() }),
+            {
+                a: Remove,
+                b: Remove,
+            },
+        );
+        expect(result.success).toBeFalsy();
+    });
+});
