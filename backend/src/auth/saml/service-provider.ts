67179dc07ef293ab78ef58b812179d0de2533592
Mia Celeste <mia@miaceleste.dev>
2023-08-01 18:07:48 -0700

    updated service-provider

--- backend/src/auth/saml/service-provider.ts
+++ backend/src/auth/saml/service-provider.ts
@@ -1,3 +1,9 @@
+/**
+ * This file defines the service provider attributes necessary for integrating with CAS. However, no change here
+ * will take any effect unless a matching metadata is uploaded to the TCCS server. To generate
+ * a new metadata, append `sp.exportMetadata("metadata.xml")`, run this file, and email the output to TCCS contact.
+ */
+
 import * as samlify from "samlify";
 import { loadDataFile } from "hyperschedule-data";
 /* The source code of this package has been manually reviewed, even though it has no readme and no star on GitHub
@@ -18,11 +24,11 @@ export const idp = samlify.IdentityProvider({
 export const sp = samlify.ServiceProvider({
     wantAssertionsSigned: true,
     entityID: "https://hyperschedule.io/",
-    nameIDFormat: ["urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified"],
+    nameIDFormat: ["urn:oasis:names:tc:SAML:1.1:nameid-format:persistent"],
     assertionConsumerService: [
         {
             Binding: "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",
-            Location: "http://dragonfruit.hyperschedule.io:8080/auth/saml",
+            Location: "https://dragonfruit.hyperschedule.io/auth/saml",
         },
     ],
 });
