f68b575cabd732cb28831863476bfab7bf1c5c7e
Mia Celeste <mia@miaceleste.dev>
2023-08-14 20:25:34 -0700

    Frontend: only apply vite fix for prod build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,18 @@ import * as path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// fix the weird thing with use-sync-external-store, imported by both zustand and react-query
+// this is only needed for production build. this module doesn't even matter because we are
+// already using react 18
+let useSyncExternalStoreFix = {};
+if (process.env.NODE_ENV === "production") {
+    useSyncExternalStoreFix = {
+        "use-sync-external-store/shim": path.dirname(
+            require.resolve("use-sync-external-store"),
+        ),
+    };
+}
+
 export default defineConfig({
     root: "src",
     envDir: __dirname,
@@ -12,10 +24,7 @@ export default defineConfig({
             "@components": path.join(__dirname, "src/components"),
             "@lib": path.join(__dirname, "src/lib"),
             "@hooks": path.join(__dirname, "src/hooks"),
-            // fix the weird thing with use-sync-external-store, imported by both zustand and react-query
-            "use-sync-external-store/shim": path.dirname(
-                require.resolve("use-sync-external-store"),
-            ),
+            ...useSyncExternalStoreFix,
         },
     },
     css: {},
