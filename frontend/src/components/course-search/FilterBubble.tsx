d838771e8f1bbe1685c5bce8bad803cf662b0182
Mia Celeste <mia@miaceleste.dev>
2023-09-06 22:26:04 -0700

    frontend: seperated autocomplete to be a different component from course area bubble

--- frontend/src/components/course-search/FilterBubble.tsx
+++ frontend/src/components/course-search/FilterBubble.tsx
@@ -131,6 +131,155 @@ function FilterBubbleTextInput(
     );
 }
 
+function AutoComplete(props: {
+    onSelect: (index: number) => void;
+    onKeyDown: (ev: React.KeyboardEvent<HTMLInputElement>) => void;
+    choices: string[];
+    render: React.FC<{ index: number }>;
+}) {
+    const [text, setText] = React.useState("");
+    const [hasFocus, setHasFocus] = React.useState(false);
+    const [selectIndex, setSelectIndex] = React.useState(0);
+    const [valid, setValid] = useState(false);
+
+    const inputRef = React.useRef<HTMLInputElement>(null);
+    const dropdownRef = React.useRef<HTMLDivElement>(null);
+
+    function recomputeFocus() {
+        setHasFocus(document.activeElement === inputRef.current);
+    }
+
+    React.useEffect(recomputeFocus, [document.activeElement, inputRef]);
+
+    function setActive(index: number) {
+        setSelectIndex(index);
+        // css nth-child is 1-indexed
+        document
+            .querySelector(`.${Css.autocompleteItem}:nth-child(${index + 1})`)!
+            .scrollIntoView(false);
+    }
+
+    const filteredChoices = React.useMemo(() => {
+        const matched: { choice: string; originalIndex: number }[] = [];
+        const tokens = text.toLocaleLowerCase().split(/\s+/);
+
+        for (let i = 0; i < props.choices.length; ++i) {
+            if (
+                !tokens.every((token) =>
+                    props.choices[i]!.toLocaleLowerCase().includes(token),
+                )
+            )
+                continue;
+            matched.push({ choice: props.choices[i]!, originalIndex: i });
+        }
+
+        // fun fact: levenshtein edit distance is equivalent to length
+        // (in the case that we are filtering candidates by substring include)
+        matched.sort((a, b) => a.choice.length - b.choice.length);
+
+        return matched;
+    }, [props.choices, text]);
+
+    return (
+        <div
+            className={Css.autocomplete}
+            data-focus={hasFocus || undefined}
+            data-isvalid={valid || undefined}
+        >
+            <div className={Css.sizer}>
+                <input
+                    ref={inputRef}
+                    size={1}
+                    value={
+                        hasFocus || !valid
+                            ? text
+                            : filteredChoices[selectIndex]?.choice ?? text
+                    }
+                    onChange={(ev) => {
+                        setText(ev.target.value);
+                        setValid(false);
+                        setSelectIndex(0);
+                    }}
+                    className={Css.input}
+                    onKeyDown={(ev) => {
+                        switch (ev.code) {
+                            case "ArrowUp":
+                                setActive(Math.max(selectIndex - 1, 0));
+                                return;
+                            case "ArrowDown":
+                                setActive(
+                                    Math.min(
+                                        selectIndex + 1,
+                                        props.choices.length - 1,
+                                    ),
+                                );
+                                return;
+                            case "Tab":
+                            case "Enter":
+                                const selected = filteredChoices[selectIndex];
+                                if (selected === undefined) return;
+                                setText(selected.choice);
+                                setValid(true);
+                                props.onSelect(selected.originalIndex);
+                                if (ev.code === "Tab")
+                                    // tab by default focuses the next element
+                                    ev.preventDefault();
+                                return;
+                            default:
+                                props.onKeyDown(ev);
+                        }
+                    }}
+                    onFocus={recomputeFocus}
+                    onBlur={() => {
+                        const selected = filteredChoices[selectIndex];
+                        if (
+                            selected !== undefined &&
+                            text.toLocaleLowerCase() ===
+                                selected.choice.toLocaleLowerCase()
+                        ) {
+                            // accept the filter if someone types the identical area, then clicks away (or press right arrow)
+                            props.onSelect(selected.originalIndex);
+                            setSelectIndex(0);
+                            setValid(true);
+                        }
+                        recomputeFocus();
+                    }}
+                />
+                <span className={Css.mirror}>{text}</span>
+            </div>
+            <div className={Css.dropdown} ref={dropdownRef}>
+                {filteredChoices.map((choice, index) => {
+                    return (
+                        <div
+                            key={choice.choice}
+                            className={Css.autocompleteItem}
+                            data-highlight={index === selectIndex || undefined}
+                            onPointerEnter={
+                                // we don't need to use setActive because we don't need to scroll
+                                () => setSelectIndex(index)
+                            }
+                            onPointerDown={
+                                // prevent default so it doesn't trigger a blur
+                                // (otherwise this dropdown would be deleted before click
+                                // and you can never click on it
+                                (ev) => ev.preventDefault()
+                            }
+                            onClick={() => {
+                                setText(choice.choice);
+                                setValid(true);
+                                props.onSelect(choice.originalIndex);
+                                setSelectIndex(0);
+                            }}
+                        >
+                            <props.render index={choice.originalIndex} />
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+
 const campusCss = [
     // this is ordered by the year the school is established
     Css.pom,
@@ -148,11 +297,11 @@ function CourseAreaBubble(
     const courseAreaDescription = useCourseAreaDescription();
     const activeSections = useActiveSectionsQuery().data;
 
-    const relevantAreas: Map<string, string> = React.useMemo(() => {
+    const relevantAreas = React.useMemo(() => {
         // not all areas exist in a semester
         const descriptions = courseAreaDescription.data;
-        if (descriptions === undefined) return new Map();
-        if (activeSections === undefined) return new Map();
+        if (descriptions === undefined) return [];
+        if (activeSections === undefined) return [];
         const areas = new Map<string, string>();
         for (const s of activeSections)
             for (const a of s.courseAreas) {
@@ -160,162 +309,39 @@ function CourseAreaBubble(
                 if (desc) areas.set(a, desc);
             }
 
-        return areas;
+        return Array.from(areas.entries()).map(([area, description]) => ({
+            area,
+            description,
+        }));
     }, [activeSections]);
 
-    const [selectedArea, setSelectedArea] = React.useState<string | null>(null);
-    const [text, setText] = React.useState("");
-
-    const inputRef = React.useRef<HTMLInputElement>(null);
-    const dropdownRef = React.useRef<HTMLDivElement>(null);
-    const [hasFocus, setHasFocus] = React.useState(false);
-
-    const [selectIndex, setSelectIndex] = React.useState(0);
-
-    function recomputeFocus() {
-        setHasFocus(document.activeElement === inputRef.current);
-    }
-
-    React.useEffect(recomputeFocus, [document.activeElement, inputRef]);
-
-    function setActive(index: number) {
-        setSelectIndex(index);
-        // css nth-child is 1-indexed
-        document
-            .querySelector(`.${Css.item}:nth-child(${index + 1})`)!
-            .scrollIntoView(false);
-    }
-
-    const matchedAreaDescriptions = React.useMemo(() => {
-        const matched: { area: string; description: string }[] = [];
-        const tokens = text.toLocaleLowerCase().split(/\s+/);
-
-        for (const [area, description] of relevantAreas.entries()) {
-            if (
-                !tokens.every((token) =>
-                    description.toLocaleLowerCase().includes(token),
-                )
-            )
-                continue;
-            matched.push({ area, description });
-        }
-
-        // fun fact: levenshtein edit distance is equivalent to length
-        // (in the case that we are filtering candidates by substring include)
-        return matched.sort(
-            (a, b) => a.description.length - b.description.length,
-        );
-    }, [relevantAreas, text]);
-
-    const selectedDescription = selectedArea
-        ? relevantAreas.get(selectedArea)
-        : undefined;
-
     return (
-        <div
-            className={Css.courseArea}
-            data-focus={hasFocus || undefined}
-            data-selected={selectedArea}
-        >
-            <div className={Css.sizer}>
-                <input
-                    ref={inputRef}
-                    size={1}
-                    value={hasFocus ? text : selectedDescription ?? text}
-                    onChange={(ev) => {
-                        setText(ev.target.value);
-                        setSelectedArea(null);
-                        setSelectIndex(0);
-                    }}
-                    className={Css.input}
-                    onKeyDown={(ev) => {
-                        switch (ev.code) {
-                            case "ArrowUp":
-                                setActive(Math.max(selectIndex - 1, 0));
-                                return;
-                            case "ArrowDown":
-                                setActive(
-                                    Math.min(
-                                        selectIndex + 1,
-                                        matchedAreaDescriptions.length - 1,
-                                    ),
-                                );
-                                return;
-                            case "Tab":
-                            case "Enter":
-                                const selected =
-                                    matchedAreaDescriptions[selectIndex];
-                                if (selected === undefined) return;
-                                setText(selected.description);
-                                setSelectedArea(selected.area);
-                                props.onChange({ area: selected.area });
-                                props.focusNext();
-                                if (ev.code === "Tab")
-                                    // tab by default focuses the next element
-                                    ev.preventDefault();
-                                return;
-                            default:
-                                props.onKeyDown(ev);
-                        }
-                    }}
-                    onFocus={recomputeFocus}
-                    onBlur={() => {
-                        const selected = matchedAreaDescriptions[selectIndex];
-                        if (
-                            selected !== undefined &&
-                            text.toLocaleLowerCase() ===
-                                selected.description.toLocaleLowerCase()
-                        ) {
-                            // accept the filter if someone types the identical area, then clicks away (or press right arrow)
-                            setSelectedArea(selected.area);
-                            props.onChange({ area: selected.area });
-                        }
-                        recomputeFocus();
-                    }}
-                />
-                <span className={Css.mirror}>{text}</span>
-            </div>
-            <div className={Css.dropdown} ref={dropdownRef}>
-                {matchedAreaDescriptions.map(({ area, description }, index) => {
-                    const campus = area.charCodeAt(0) - 48 - 1; // 48 is the ascii code for '0'
-
-                    return (
-                        <div
-                            key={area}
-                            className={Css.item}
-                            data-highlight={index === selectIndex || undefined}
-                            onPointerEnter={
-                                // we don't need to use setActive because we don't need to scroll
-                                () => setSelectIndex(index)
-                            }
-                            onPointerDown={
-                                // prevent default so it doesn't trigger a blur
-                                // (otherwise this dropdown would be deleted before click
-                                // and you can never click on it
-                                (ev) => ev.preventDefault()
-                            }
-                            onClick={() => {
-                                setText(description);
-                                setSelectedArea(area);
-                                props.onChange({ area });
-                                props.focusNext();
-                            }}
-                        >
-                            {campus < 5 ? (
-                                <span
-                                    className={classNames(
-                                        campusCss[campus],
-                                        Css.campusIndicator,
-                                    )}
-                                />
-                            ) : (
-                                <></>
-                            )}
-                            <span>{description}</span>
-                        </div>
-                    );
-                })}
-            </div>
-        </div>
+        <AutoComplete
+            onSelect={(index) => {
+                props.onChange(relevantAreas[index] ?? null);
+                props.focusNext();
+            }}
+            onKeyDown={props.onKeyDown}
+            choices={relevantAreas.map((v) => v.description)}
+            render={({ index }) => {
+                const { area, description } = relevantAreas[index]!;
+                const campus = area.charCodeAt(0) - 48 - 1; // 48 is the ascii code for '0'
+                return (
+                    <>
+                        {campus < 5 ? (
+                            <span
+                                className={classNames(
+                                    campusCss[campus],
+                                    Css.campusIndicator,
+                                )}
+                            />
+                        ) : (
+                            <></>
+                        )}
+                        <span>{description}</span>
+                    </>
+                );
+            }}
+        />
     );
 }
