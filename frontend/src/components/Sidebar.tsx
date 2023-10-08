13fb19a66f5d36ebedb3edf8f4eb88706d81dd8f
Mia Celeste <mia@miaceleste.dev>
2023-10-08 02:36:29 -0700

    frontend: moved schedule selector to sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -3,10 +3,17 @@ import MiniMap from "@components/MiniMap";
 import SelectedList from "@components/SelectedList";
 import * as React from "react";
 import useStore, { MainTab } from "@hooks/store";
+import { useUserStore } from "@hooks/store/user";
 import ScheduleControl from "@components/schedule/ScheduleControl";
 import { shallow } from "zustand/shallow";
 import * as Feather from "react-feather";
 import ThemeSlider from "./ThemeSlider";
+import Dropdown from "@components/common/Dropdown";
+import * as APIv4 from "hyperschedule-shared/api/v4";
+
+function scheduleDisplayName(schedule: APIv4.UserSchedule) {
+    return `${schedule.name} (${APIv4.stringifyTermIdentifier(schedule.term)})`;
+}
 
 export default function Sidebar() {
     const { tab, show, setShow } = useStore(
@@ -17,6 +24,19 @@ export default function Sidebar() {
         }),
         shallow,
     );
+
+    const user = useUserStore();
+
+    const confirmedGuest = useUserStore((user) => user.hasConfirmedGuest);
+    const serverData = useUserStore((user) => user.server);
+
+    const schedules = APIv4.getSchedulesSorted(user.schedules);
+
+    const scheduleChoices = schedules.map((s) => scheduleDisplayName(s[1]));
+    const selectedSchedule =
+        user.activeScheduleId === null
+            ? ""
+            : scheduleDisplayName(user.schedules[user.activeScheduleId]!);
 
     return (
         <>
@@ -44,6 +64,31 @@ export default function Sidebar() {
                     <ScheduleControl />
                 )}
                 <SelectedList />
+
+                {serverData === null && !confirmedGuest ? (
+                    <></>
+                ) : (
+                    <div className={Css.scheduleSelect}>
+                        <div className={Css.dropdownContainer}>
+                            <span>Select a schedule:</span>
+                            <Dropdown
+                                choices={scheduleChoices}
+                                selected={selectedSchedule}
+                                emptyPlaceholder="no schedule selected"
+                                onSelect={(index) =>
+                                    user.setActiveScheduleId(
+                                        schedules[index]![0],
+                                    )
+                                }
+                            />
+                        </div>
+                        <button className={Css.editScheduleButton}>
+                            <Feather.Edit className={Css.editIcon} />
+                            edit
+                        </button>
+                    </div>
+                )}
+
                 <ThemeSlider />
             </div>
         </>
--- frontend/src/components/Toolbar.tsx
+++ frontend/src/components/Toolbar.tsx
@@ -1,33 +1,18 @@
 import { useUserStore } from "@hooks/store/user";
-import useStore from "@hooks/store";
-import { PopupOption } from "@lib/popup";
-import * as APIv4 from "hyperschedule-shared/api/v4";
+
 import Css from "./Toolbar.module.css";
 import { CURRENT_TERM } from "hyperschedule-shared/api/current-term";
-import Dropdown from "@components/common/Dropdown";
-import * as Feather from "react-feather";
-import { pick } from "@lib/store";
 
-function scheduleDisplayName(schedule: APIv4.UserSchedule) {
-    return `${schedule.name} (${APIv4.stringifyTermIdentifier(schedule.term)})`;
-}
+import * as Feather from "react-feather";
 
 export default function Toolbar() {
     const loggedIn = useUserStore((user) => user.server);
-    const confirmedGuest = useUserStore((user) => user.hasConfirmedGuest);
 
     function loginThroughCAS() {
         window.location.href = `${__API_URL__}/auth/saml`;
     }
 
     const user = useUserStore();
-    const schedules = APIv4.getSchedulesSorted(user.schedules);
-
-    const scheduleChoices = schedules.map((s) => scheduleDisplayName(s[1]));
-    const selectedSchedule =
-        user.activeScheduleId === null
-            ? ""
-            : scheduleDisplayName(user.schedules[user.activeScheduleId]!);
 
     return (
         <div className={Css.toolbar}>
@@ -41,18 +26,6 @@ export default function Toolbar() {
             <button>Report issues</button>
             <button>Export calendar</button>
             <Feather.GitHub />
-            {confirmedGuest || loggedIn ? (
-                <Dropdown
-                    choices={scheduleChoices}
-                    selected={selectedSchedule}
-                    emptyPlaceholder="no schedule selected"
-                    onSelect={(index) =>
-                        user.setActiveScheduleId(schedules[index]![0])
-                    }
-                />
-            ) : (
-                <></>
-            )}
 
             {loggedIn ? (
                 "logged in"
