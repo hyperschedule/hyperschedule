dff088cd903e4bf90555c5c1604055793865c90a
Mia Celeste <mia@miaceleste.dev>
2023-10-18 15:49:52 -0700

    frontend: replaced json-viewer package and implemented a minimal cloud data viewer

--- frontend/src/errors/json-viewer.tsx
+++ frontend/src/errors/json-viewer.tsx
@@ -1,40 +1,70 @@
 import { createRoot } from "react-dom/client";
 
-import { JsonView, allExpanded, defaultStyles } from "react-json-view-lite";
-import "react-json-view-lite/dist/index.css";
-import { MAIN_STORE_NAME, USER_STORE_NAME } from "@lib/constants";
+import JsonView from "react18-json-view";
+import "react18-json-view/src/style.css";
 
-// we will only ever render this if there's a repeated UI crash.
-// do not use any hyperschedule UI component here
+import { MAIN_STORE_NAME, USER_STORE_NAME } from "@lib/constants";
+import { useState } from "react";
+
+// this is rendered if there's a repeated UI crash or the user tried to view their data.
+// do not use any hyperschedule UI component here.
 function JSONViewer() {
+    const [cloudData, setCloudData] = useState<
+        Record<string, unknown> | string | undefined | null
+    >(undefined);
+
     if (window.location.hash !== "") {
         const data = JSON.parse(atob(window.location.hash.slice(1)));
-        return (
-            <JsonView
-                data={data}
-                shouldExpandNode={allExpanded}
-                style={defaultStyles}
-            />
-        );
+        return <JsonView src={data} displaySize="collapsed" />;
+    }
+
+    function retrieveCloud() {
+        (async function () {
+            // no schemaFetch because import might be unstable
+            const headers = await fetch(`${__API_URL__}/v4/user`, {
+                credentials: "include",
+            });
+            if (headers.status === 401) {
+                setCloudData("Not logged in");
+            } else if (!headers.ok) {
+                setCloudData("Network error");
+            } else {
+                setCloudData(await headers.json());
+            }
+        })().catch(() => setCloudData("Failed"));
     }
 
     return (
-        <div style={{ fontFamily: "Inter, sans-serif" }}>
-            <h3>Interface Data</h3>
+        <div>
+            <h3>Cloud data</h3>
+            {cloudData === undefined ? (
+                <button onClick={retrieveCloud}>Retrieve</button>
+            ) : cloudData === null ? (
+                "pending"
+            ) : typeof cloudData === "string" ? (
+                cloudData
+            ) : (
+                <JsonView src={cloudData} displaySize="collapsed" />
+            )}
+
+            <h3>Persistent interface data (local)</h3>
             <JsonView
-                data={JSON.parse(localStorage.getItem(MAIN_STORE_NAME) ?? "{}")}
-                shouldExpandNode={allExpanded}
-                style={defaultStyles}
+                src={JSON.parse(localStorage.getItem(MAIN_STORE_NAME) ?? "{}")}
+                displaySize="collapsed"
             />
 
-            <h3>User Data</h3>
+            <h3>Persistent user data (local)</h3>
             <JsonView
-                data={JSON.parse(localStorage.getItem(USER_STORE_NAME) ?? "{}")}
-                shouldExpandNode={allExpanded}
-                style={defaultStyles}
+                src={JSON.parse(localStorage.getItem(USER_STORE_NAME) ?? "{}")}
+                displaySize="collapsed"
             />
         </div>
     );
 }
 
-createRoot(document.getElementById("root")!).render(<JSONViewer />);
+createRoot(document.getElementById("root")!).render(
+    // line-height is needed for https://github.com/YYsuni/react18-json-view/issues/24
+    <div style={{ lineHeight: "1.25em", fontFamily: "Inter, sans-serif" }}>
+        <JSONViewer />
+    </div>,
+);
