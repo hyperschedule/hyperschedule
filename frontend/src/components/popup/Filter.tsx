08fc0035ed5121f7146afb54f1f533e77cb08c61
Mia Celeste <mia@miaceleste.dev>
2023-10-28 17:34:14 -0700

    frontend: re-layout filter popup

--- frontend/src/components/popup/Filter.tsx
+++ frontend/src/components/popup/Filter.tsx
@@ -1,8 +1,10 @@
 import * as Css from "./Filter.module.css";
+import * as AppCss from "@components/App.module.css";
 import { ExampleFilterBubble } from "@components/course-search/filter-bubble/FilterBubble";
 import * as Search from "@lib/search";
-import * as Feather from "react-feather";
 import useStore from "@hooks/store";
+import { memo, Fragment } from "react";
+import classNames from "classnames";
 
 type FilterSpec = {
     name: string;
@@ -56,7 +58,7 @@ const filterSpecs: { [k in Search.FilterKey]: FilterSpec } = {
     },
 };
 
-export default function Filter() {
+export default memo(function Filter() {
     const setPopup = useStore((store) => store.setPopup);
     const addFilter = useStore((store) => store.addSearchFilter);
 
@@ -64,75 +66,62 @@ export default function Filter() {
         <div>
             <h2 className={Css.title}> Filters </h2>
             <div>
-                When multiple filters are specified, only courses matching all
-                the criteria will be shown. You can add a filter by typing its
-                left-side, followed by a colon, into the search box. For
-                example, you can add{" "}
-                <span style={{ display: "inline-block", width: "fit-content" }}>
-                    <ExampleFilterBubble filterKey="dept" data="CS" />
-                </span>{" "}
-                by typing <mark>dept:</mark> into the search box. All the
-                examples here will match Mudd's CS5 in FA2023. All filters are
-                case-insensitive.
+                All filters are case-insensitive. When multiple filters are
+                specified, only courses matching all the criteria will be shown
+                (logical AND).
             </div>
-            <div className={Css.tableWrapper}>
-                <table className={Css.filterTable}>
-                    <thead>
-                        <tr>
-                            <th>Filter for</th>
-                            <th>Type into search box</th>
-                            <th>Examples</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {(
-                            Object.entries(filterSpecs) as [
-                                Search.FilterKey,
-                                FilterSpec,
-                            ][]
-                        ).map(([key, spec]) => {
-                            return (
-                                <tr key={key}>
-                                    <td className={Css.nameColumn}>
-                                        <Feather.PlusCircle
-                                            className={Css.addIcon}
-                                            onClick={() => {
-                                                setPopup(null);
-                                                addFilter({ key, data: null });
-                                            }}
+            <div className={Css.filterTable}>
+                <span className={Css.tableHeader}>Filter for</span>
+                <span className={Css.tableHeader}>Type into search box</span>
+                <span className={Css.tableHeader}>Examples</span>
+
+                {(
+                    Object.entries(filterSpecs) as [
+                        Search.FilterKey,
+                        FilterSpec,
+                    ][]
+                ).map(([key, spec], index) => {
+                    return (
+                        <Fragment key={key}>
+                            <span
+                                className={Css.line}
+                                style={{
+                                    gridRow: index + 2,
+                                }}
+                            />
+                            <div className={Css.nameWrapper}>
+                                <span className={Css.name}>{spec.name}</span>
+                                <button
+                                    className={classNames(
+                                        AppCss.defaultButton,
+                                        Css.applyButton,
+                                    )}
+                                    onClick={() => {
+                                        setPopup(null);
+                                        addFilter({ key, data: null });
+                                    }}
+                                >
+                                    Apply
+                                </button>
+                            </div>
+                            <span className={Css.typeable}>
+                                <code>{key}:</code>
+                            </span>
+
+                            <div className={Css.examples}>
+                                {spec.example.map((s) => (
+                                    <div className={Css.bubbleWrapper} key={s}>
+                                        <ExampleFilterBubble
+                                            filterKey={key}
+                                            data={s}
                                         />
-                                        <span className={Css.name}>
-                                            {spec.name}
-                                        </span>
-                                    </td>
-
-                                    <td className={Css.typeable}>
-                                        <code>{key}:</code>
-                                    </td>
-
-                                    <td>
-                                        <div className={Css.example}>
-                                            {spec.example.map((s) => (
-                                                <div
-                                                    className={
-                                                        Css.bubbleWrapper
-                                                    }
-                                                    key={s}
-                                                >
-                                                    <ExampleFilterBubble
-                                                        filterKey={key}
-                                                        data={s}
-                                                    />
-                                                </div>
-                                            ))}
-                                        </div>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
+                                    </div>
+                                ))}
+                            </div>
+                        </Fragment>
+                    );
+                })}
             </div>
         </div>
     );
-}
+});
